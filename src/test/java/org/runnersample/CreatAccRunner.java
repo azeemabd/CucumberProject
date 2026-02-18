b2c0768d05220b3707146f9fd6bd628a77a29046
azeemabd <abdulazeem2046@gmail.com>
2026-02-18 19:37:45 +0530

    Added cucumber report configuration

--- src/test/java/org/runnersample/CreatAccRunner.java
+++ src/test/java/org/runnersample/CreatAccRunner.java
@@ -1,13 +1,19 @@
 package org.runnersample;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/FeatureFiles",
-glue="org.createacc",dryRun=false,tags="@smoke")
-public class CreatAccRunner {
+@CucumberOptions(
+        features = "src/test/resources",
+        glue = "org.step",
+        plugin = {
+                "pretty",
+                "json:target/cucumber.json",
+                "html:target/cucumber-report.html"
+        }
+)
 
+public class CreatAccRunner {
 }
